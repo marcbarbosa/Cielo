0a2af4ea84a9cae31c6bf5726b9ddf0aed2f40c3
Marcelo Barbosa <mbarbosa@gmail.com>
2015-01-31 11:59:31 -0200

    Adding tests project

---
+++ Cielo.Facts/CieloClientFacts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cielo.Facts
+{
+    public class CieloClientFacts
+    {
+        public class Constructor
+        {
+            [Fact]
+            public void ShoudReturnArgumentNullExceptionGivenNoConfigSettings()
+            {
+                // arrange
+                // act
+                //assert
+                Assert.Throws<ArgumentNullException>(() => new CieloClient());
+            }
+        }
+
+        public class AutorizarTransacao
+        {
+            [Fact(Skip="Refactoring...")]
+            public void ShouldReturnNullGivenNullTid()
+            { 
+                // arrange
+                var cielo = new CieloClient();
+
+                // act
+                var actual = cielo.AutorizarTransacao(null);
+
+                //assert
+                Assert.Null(actual);
+            }
+        }
+    }
+}
